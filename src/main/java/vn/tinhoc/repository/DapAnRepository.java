a81a30b514e86321499137657e76303d2168792b
NguyenDinhHoangLan <63280056+LanHoangNTU@users.noreply.github.com>
2021-11-04 20:46:05 +0700

    API call tutorials

---
+++ src/main/java/vn/tinhoc/repository/DapAnRepository.java
@@ -0,0 +1,11 @@
+package vn.tinhoc.repository;
+
+import org.springframework.stereotype.Repository;
+
+import vn.lanhoang.ontology.repository.OntologyRepository;
+import vn.tinhoc.domain.DapAn;
+
+@Repository
+public class DapAnRepository extends OntologyRepository<DapAn> {
+
+}
