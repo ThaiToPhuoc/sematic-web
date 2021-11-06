0882014b1876a597e01afb79cf9dcb7c24f42f04
NguyenDinhHoangLan <63280056+LanHoangNTU@users.noreply.github.com>
2021-11-06 20:11:58 +0700

    Revert "Revert "Tut 3""
    This reverts commit adbefc4c2509c1fb009deb7d5b5cce7b6c531867.

--- src/main/java/vn/tinhoc/domain/CauHoi.java
+++ src/main/java/vn/tinhoc/domain/CauHoi.java
@@ -1,7 +1,5 @@
 package vn.tinhoc.domain;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import vn.lanhoang.ontology.annotation.Name;
 import vn.lanhoang.ontology.annotation.OntologyObject;
 
@@ -27,15 +25,15 @@ public class CauHoi {
 		return NoiDungCauHoi;
 	}
 
-	public void setNoiDungCauHoi(String noiDungCauHoi) {
-		NoiDungCauHoi = noiDungCauHoi;
+	public void setNoiDungCauHoi(String NoiDungCauHoi) {
+		this.NoiDungCauHoi = NoiDungCauHoi;
 	}
 
 	public Integer getSTTCauHoi() {
 		return STTCauHoi;
 	}
 
-	public void setSTTCauHoi(Integer sTTCauHoi) {
-		STTCauHoi = sTTCauHoi;
+	public void setSTTCauHoi(Integer STTCauHoi) {
+		this.STTCauHoi = STTCauHoi;
 	}
 }
