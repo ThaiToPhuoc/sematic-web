93acce4d8baabeeeeefcf81af4e1a827598faa6d
ThaiToPhuoc <63282346+ThaiToPhuoc@users.noreply.github.com>
2021-11-19 20:56:22 +0700

    xóa log

--- src/main/app/src/pages/public/BaiGiangIndex.js
+++ src/main/app/src/pages/public/BaiGiangIndex.js
@@ -24,7 +24,6 @@ export default class BaiGiangIndex extends Component {
         PublicService.listBaiGiang()
         .then(response => {
             if (response?.data) {
-                console.log(response)
                 this.setState({
                     BaiGiang: response.data
                 })
