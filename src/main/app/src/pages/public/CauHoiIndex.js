dd402618a59b26702c9ca957c304ddd31613bfb7
ThaiToPhuoc <63282346+ThaiToPhuoc@users.noreply.github.com>
2021-11-20 21:51:26 +0700

    xoa log

--- src/main/app/src/pages/public/CauHoiIndex.js
+++ src/main/app/src/pages/public/CauHoiIndex.js
@@ -19,7 +19,6 @@ export default class CauHoiIndex extends Component {
         PublicService.listCauHoi(this.state.id)
         .then(response => {
             if (response?.data) {
-                console.log(response)
                 this.setState({
                     cauHoiDTOs: response.data
                 })
