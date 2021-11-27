3923482ee8d94526b8c70704f32df217c559f8a5
NguyenDinhHoangLan <63280056+LanHoangNTU@users.noreply.github.com>
2021-11-27 17:41:00 +0700

    Nop bai - BE

---
+++ src/main/java/vn/tinhoc/domain/dto/NopBaiDTO.java
@@ -0,0 +1,23 @@
+package vn.tinhoc.domain.dto;
+
+public class NopBaiDTO {
+    private String cauHoi;
+
+    private String dapAn;
+
+    public String getCauHoi() {
+        return cauHoi;
+    }
+
+    public void setCauHoi(String cauHoi) {
+        this.cauHoi = cauHoi;
+    }
+
+    public String getDapAn() {
+        return dapAn;
+    }
+
+    public void setDapAn(String dapAn) {
+        this.dapAn = dapAn;
+    }
+}
